d35c0411a4be7b7adccf26d44cd6daae51a06a7d
JuliaHFerreira <juliahelena317@gmail.com>
2026-02-13 19:30:53 -0300

    feat: suggest producible quantities and total value prioritizing highest-value products

--- src/main/java/com/stockcontrol/RawMaterial/view/ProductCapacityView.java
+++ src/main/java/com/stockcontrol/RawMaterial/view/ProductCapacityView.java
@@ -4,6 +4,7 @@ public interface ProductCapacityView {
     String getProductCode();
     String getProductDescription();
     Long getMaxProducible();
+    Double getTotalPrice();
+    Integer getPriority();
     String getStructureStatus();
 }
-
